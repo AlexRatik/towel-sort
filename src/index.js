0127d2d3c9c9d640efd1a1e4731d0d29a5536489
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-07-10 16:58:56 +0300

    feat: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) return [];
+    let result = [];
+    matrix.reduce(function (prev, current, index) {
+        if (index % 2 === 1) {
+            current.reverse().forEach((item) => result.push(item));
+        } else {
+            current.forEach((item) => {
+                result.push(item);
+            });
+        }
+    }, matrix[0]);
+    return result;
+};
